758ccea47ff910f8afd29b5de25b2bcfcc25fcd4
Kumar Tambe <kumartambe49@gmail.com>
2026-08-27 17:58:36 +0530

    started trees - solved 1 Q - inorderTraversal

---
+++ 06_Trees.cpp/inorderTraversal.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void helper(TreeNode* root,vector<int>&result) {
+    if(root == nullptr) return;
+    helper(root->left,result);
+    result.push_back(root->val);
+    helper(root->right,result);
+}
+
+vector<int> inorderTraversal(TreeNode* root){
+    vector<int> result;
+    helper(root,result);
+    return result;
+}
