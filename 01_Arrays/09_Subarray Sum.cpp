f692602e9f27dc959c7c4838584e4abd386e807e
Kumar Tambe <kumartambe49@gmail.com>
2026-08-29 15:33:29 +0530

    updated subarray maxSum

--- 01_Arrays/09_Subarray Sum.cpp
+++ 01_Arrays/09_Subarray Sum.cpp
@@ -4,24 +4,16 @@ using namespace std;
 
 int subarraySum(vector<int> &nums)
 {
-    int currentSum = nums[0];
-    int maxSum = nums[0];
-    for (int i = 1; i < nums.size(); i++)
-    {
-        if (currentSum < 0)
-        {
-            currentSum = nums[i];
-        }
-        else
-        {
+    int currentSum = 0;
+        int maxSum = nums[0];
+        for(int i=0;i<nums.size();i++){
             currentSum += nums[i];
+            maxSum = max(currentSum,maxSum);
+            if( currentSum < 0){
+                currentSum = 0;
+            }
         }
-        if (currentSum > maxSum)
-        {
-            maxSum = currentSum;
-        }
-    }
-    return maxSum;
+        return maxSum;
 }
 
 int main()
