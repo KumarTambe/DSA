dcaeec0a102e35b8102cf01421a7646b5bbdf5d1
Kumar Tambe <kumartambe49@gmail.com>
2026-09-21 17:07:13 +0530

    solved 2 product except self Qs

--- 01_Arrays/14_ProductOfArrExceptSelf.cpp
+++ 01_Arrays/14_ProductOfArrExceptSelf.cpp
@@ -1,41 +1,23 @@
-// LeetCode #238 - Product of Array Except Self
-#include <vector>
-#include <iostream>
-using namespace std;
 
-vector<int> multiplyElement(vector<int> &nums)
-{
-    int n = nums.size();
-
-    vector<int> leftProducts(n);
-    leftProducts[0] = 1;
-    for (int i = 1; i < n; i++)
-    {
-        leftProducts[i] = leftProducts[i - 1] * nums[i - 1];
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> result(n,0);
+        vector<int> left(n,0);
+        vector<int> right(n,0);
+        left[0] = 1;
+        right[n-1] = 1;
+        for(int i = 1;i<n;i++){
+            left[i] = left[i-1] * nums[i-1];
+        }
+        for(int j = n-2;j>=0;j--){
+            right[j] = right[j+1] * nums[j+1];
+        }
+        for(int i=0;i<n;i++){
+            result[i] = left[i] * right[i];
+        }
+        return result;
     }
+};
 
-    vector<int> rightProducts(n);
-    rightProducts[n - 1] = 1;
-    for (int i = n - 2; i >= 0; i--)
-    {
-        rightProducts[i] = rightProducts[i + 1] * nums[i + 1];
-    }
-
-    vector<int> result(n);
-    for (int i = 0; i < n; i++)
-    {
-        result[i] = leftProducts[i] * rightProducts[i];
-    }
-
-    return result;
-}
-
-int main()
-{
-    vector<int> nums = {4, 5, 2};
-    vector<int> arr = multiplyElement(nums);
-    for (auto x : arr)
-    {
-        cout << x << endl;
-    }
-}
--- 01_Arrays/19_pivotElement.cpp
+++ 01_Arrays/19_pivotElement.cpp
@@ -1,15 +1,20 @@
-#include<bits/stdc++.h>
-using namespace std;
-
+```cpp
+class Solution {
+public:
     int pivotIndex(vector<int>& nums) {
         int totalSum = 0;
         for(auto x : nums){
             totalSum += x;
         }
         int leftSum = 0;
-        for(int i =0;i<nums.size();i++){
-            if(leftSum == totalSum - leftSum - nums[i]) return i;
-            leftSum += nums[i];
+        for(int i=0;i<nums.size();i++){
+            if(leftSum == totalSum - leftSum - nums[i]){
+                return i;
+            } else {
+                leftSum += nums[i];
+            }
         }
         return -1;
     }
+};
+```
